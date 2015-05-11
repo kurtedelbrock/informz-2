8af80d3e094204b04f9b629092b854c2dc816e15
Kurt Edelbrock <kurtedelbrock@gmail.com>
2015-05-11 14:08:22 -0400

    FizzBuzz with array work

--- app/modules/FizzBuzz.rb
+++ app/modules/FizzBuzz.rb
@@ -1,6 +1,11 @@
 module FizzBuzz
   def self.check(num)
-    val = {"FizzBuzz" => 15, "Fizz" => 3, "Buzz" => 5}.detect {|key, value| num % value == 0 }
-    val ? val.first : ""
+    
+    (f = [["Fizz"][num % 3], ["Buzz"][num % 5]].join).empty? ? "" : f
+    
+    # val = {"FizzBuzz" => 15, "Fizz" => 3, "Buzz" => 5}.detect {|key, value| num % value == 0 }
+    #
+    # val ? val.first : ""
+    
   end
 end
